7052977cfededcb9d372af42154a46df12af0d1f
Esteban Velasquez <eavc.30@gmail.com>
2022-04-16 12:16:51 -0500

    feat(back): add main function to test created classes

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-'use strict';
+"use strict";
+
+const Block = require("./src/block");
+const Blockchain = require("./src/blockchain");
+
+async function main() {
+  // Create a new blockchain.
+  const blockchain = await new Blockchain();
+
+  // Add blocks to the chain.
+  await blockchain.addBlock(new Block({ data: "Block 1" }));
+  await blockchain.addBlock(new Block({ data: "Block 2" }));
+  await blockchain.addBlock(new Block({ data: "Block 3" }));
+  await blockchain.addBlock(new Block({ data: "Block 4" }));
+
+  // Call print method to print the chain.
+  blockchain.print();
+}
+
+// Call main function.
+main();
