a63bbe2e147ef8b4fe11bc8b3fa8c11a9e6789db
Esteban Velasquez <eavc.30@gmail.com>
2022-05-13 14:59:22 -0500

    feat(front): add functions that make requests
    - function to get contacts or single contact, and add, update or delete.

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,73 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const url = 'http://localhost:3001/contacts';
+const headers = {
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+};
 
 const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [newContact, setNewContact] = useState();
+
+  useEffect(() => {
+    getContacts();
+  }, []);
+
+  const getContacts = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    setContacts(data);
+    setNewContact('');
+  };
+
+  const addContact = async () => {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({ name: newContact.name, phone: newContact.phone }),
+    });
+
+    const data = await response.json();
+
+    setContacts([...contacts, data]);
+    setNewContact('');
+  };
+
+  const deleteContact = async (id) => {
+    const response = await fetch(`${url}/${id}`, {
+      method: 'DELETE',
+    });
+
+    const data = await response.json();
+
+    setContacts(contacts.filter((contact) => contact.id !== id));
+  };
+
+  const updateContact = async (id) => {
+    const response = await fetch(`${url}/${id}`, {
+      method: 'PUT',
+      headers,
+      body: JSON.stringify({ name: newContact.name, phone: newContact.phone }),
+    });
+
+    const data = await response.json();
+
+    setContacts(
+      contacts.map((contact) => (contact.id === id ? data : contact))
+    );
+    setNewContact('');
+  };
+
+  const getContact = async (id) => {
+    const response = await fetch(`${url}/${id}`);
+    const data = await response.json();
+
+    setNewContact(data.name);
+  };
+
   return (
     <div className="App">
       <div className="App-content">
