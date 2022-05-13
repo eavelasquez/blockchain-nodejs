aee529d6c3802753cd1709e83a5569132bb430c7
Esteban Velasquez <eavc.30@gmail.com>
2022-05-13 14:07:35 -0500

    feat(back): add routes.js file
    - add CRUD for contacts.

---
+++ api/routes.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const routes = (app, db, accounts, contacts) => {
+  app.get("/", (_req, res) => {
+    res.send("Hello World! I am a contact API");
+  });
+
+  app.post("/contacts", async (req, res) => {
+    const { name, phone } = req.body;
+    const contact = await contacts.methods.addContact(name, phone).send({
+      from: accounts[0],
+    });
+
+    res.json(contact);
+  });
+
+  app.get("/contacts", (req, res) => {
+    let cache = [];
+    const count = await contacts.methods.count().call();
+
+    for (let i = 0; i < count; i++) {
+      const contact = await contacts.methods.contacts(i).call();
+      cache = [...cache, contact];
+    }
+
+    res.json(cache);
+  });
+
+  app.get("/contacts/:id", async (req, res) => {
+    const { id } = req.params;
+    const contact = await contacts.methods.contacts(id).call();
+
+    res.json(contact);
+  });
+
+  app.put("/contacts/:id", async (req, res) => {
+    const { id } = req.params;
+    const { name, phone } = req.body;
+    const contact = await contacts.methods.updateContact(id, name, phone).send({
+      from: accounts[0],
+    });
+
+    res.json(contact);
+  });
+
+  app.delete("/contacts/:id", async (req, res) => {
+    const { id } = req.params;
+    const contact = await contacts.methods.removeContact(id).send({
+      from: accounts[0],
+    });
+
+    res.json(contact);
+  });
+};
+
+module.exports = routes;
